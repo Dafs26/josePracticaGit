f3b5c7e81fedba458437a4f595e1467cb65b5f15
David Fernández <Dafs@localhost.localdomain>
2025-02-12 21:56:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+	print("Este programa suma dos numeros")
+	num1 = input("Escriba primer numero: ")
+	num2 = input("Escriba segundo numero: ")
+	print("El resultado es: " + num1 + num2)
+
+if __name__ == '__main__':
+	main()
